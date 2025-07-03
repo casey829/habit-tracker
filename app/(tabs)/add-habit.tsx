c9753cbcd79b3935378812d8a4f27bd1d5903248
casey829 <caseymwangi31@gmail.com>
2025-07-03 13:34:35 +0300

    creating the addhabit Ui

--- app/(tabs)/add-habit.tsx
+++ app/(tabs)/add-habit.tsx
@@ -1,12 +1,53 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { Button, SegmentedButtons, TextInput } from "react-native-paper";
+
+const FREQUENCIES = [
+  { label: "Daily", value: "daily" },
+  { label: "Weekly", value: "weekly" },
+  { label: "Monthly", value: "monthly" },
+];
 
 const Addhabit = () => {
   return (
-    <View>
-      <Text>add-habit</Text>
+    <View style={styles.container}>
+      <TextInput label="Title" mode="outlined" style={styles.input} />
+      <TextInput label="Description" mode="outlined" style={styles.input} />
+      <View style={styles.frequency}>
+        <SegmentedButtons
+          buttons={FREQUENCIES}
+          style={styles.segmentedButtons}
+        />
+      </View>
+      <Button mode="contained" style={styles.button}>
+        Add Habit
+      </Button>
     </View>
-  )
-}
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "f5f5f5",
+    textAlign: "center",
+    justifyContent: "center",
+  },
+  input: {
+    marginBottom: 16,
+    backgroundColor: "f5f5f5",
+  },
+  frequency: {
+    marginBottom: 24,
+    backgroundColor: "f5f5f5",
+  },
+  segmentedButtons: {
+    marginBottom: 16,
+  },
+  button: {
+    marginTop: 16,
+  },
+});
 
 export default Addhabit;
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,8 @@
 import { AuthProvider, useAuth } from "@/lib/auth-context";
 import { Stack, useRouter, useSegments } from "expo-router";
 import React, { useEffect } from "react";
+import { PaperProvider } from "react-native-paper";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 function RouteGuard({ children }: { children: React.ReactNode }) {
   const router = useRouter();
@@ -32,11 +34,15 @@ function RouteGuard({ children }: { children: React.ReactNode }) {
 export default function RootLayout() {
   return (
     <AuthProvider>
+      <PaperProvider>
+      <SafeAreaProvider>
       <RouteGuard>
         <Stack>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         </Stack>
       </RouteGuard>
+      </SafeAreaProvider>
+      </PaperProvider>
     </AuthProvider>
   );
 }
